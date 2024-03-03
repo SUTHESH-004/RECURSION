d0a4f3e5c7d6e4e9c230a4da9b36fd64d090054d
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-03-03 17:33:52 +0530

    minor change

--- Summation_Of_N_numbers.java
+++ Summation_Of_N_numbers.java
@@ -4,9 +4,7 @@ public class Summation_Of_N_numbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(sum(sc.nextInt(), 0, 1));
-
         sc.close();
-
     }
 
     static int sum(int val, int ans, int temp) {
