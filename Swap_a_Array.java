5f479cfaf376ed4df77c47923f776a3aae547b46
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-03-03 18:36:49 +0530

    .

--- Swap_a_Array.java
+++ Swap_a_Array.java
@@ -1,19 +1,19 @@
 import java.util.*;
+
 public class Swap_a_Array {
-    public static void main(String[] args)
-    {
-        int arr[] = new int[]{1,2,3,4,5};
-        System.out.println(Arrays.toString(reverse(arr,0,arr.length-1)));
+    public static void main(String[] args) {
+        int arr[] = new int[] { 1, 2, 3, 4, 5 };
+        System.out.println(Arrays.toString(reverse(arr, 0, arr.length - 1)));
         System.out.println(Arrays.toString(arr));
 
     }
-    static int[] reverse(int arr[],int l,int r)
-    {
-        if(l>=r)
+
+    static int[] reverse(int arr[], int l, int r) {
+        if (l >= r)
             return arr;
         int temp = arr[l];
-        arr[l]=arr[r];
-        arr[r]= temp;
-        return reverse(arr,l+1,r-1);
+        arr[l] = arr[r];
+        arr[r] = temp;
+        return reverse(arr, l + 1, r - 1);
     }
 }
