e32cf8cc337285804d24f2dde551350edb979fb7
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-03-03 18:03:04 +0530

    minor changes

--- functionalSumaztion.java
+++ functionalSumaztion.java
@@ -4,20 +4,20 @@ public class functionalSumaztion {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print(printSumaztion(3));
-        
+
         sc.close();
 
     }
 
     public static int printSumaztion(int ans) {
-        if(ans==1)
-        return 1;
-        return ans + printSumaztion(ans-1);
+        if (ans == 1)
+            return 1;
+        return ans + printSumaztion(ans - 1);
     }
     // 3+printSumaztion(3-1=2)
     // 2 + printSumation(2-1=1 )
-    //if 1 return 1;
+    // if 1 return 1;
     // return 2 + 1
-    //return 3+3;
-    //return 6;
+    // return 3+3;
+    // return 6;
 }
